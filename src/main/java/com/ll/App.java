09c2765d495dc88e53591ae65e9e08a39f4c06e8
win5948 <win5948@gmail.com>
2023-11-07 17:18:54 +0900

    2단계

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -12,8 +12,16 @@ public class App {
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
 
-            if(cmd.equals("종료")) {
+            if (cmd.equals("종료")) {
                 break;
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = scanner.nextLine();
+
+                System.out.print("작가 : ");
+                String authorName = scanner.nextLine();
+
+                System.out.printf("명언 : %s, 작가 : %s\n" , content, authorName);
             }
         }
     }
